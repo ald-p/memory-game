7056a24e70c6b80ba17c7fe02e83caae41cba9cc
Aldwin <aldwin.pag@gmail.com>
2023-07-03 13:51:51 -0400

    Add fucntionality to game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gameContainer = document.getElementById("game");
+const startBtn = document.getElementById("start-btn");
 
 const COLORS = [
   "red",
@@ -12,6 +13,10 @@ const COLORS = [
   "orange",
   "purple"
 ];
+
+const currentCards = [];
+let pairsMatched = 0;
+let score = 0;
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -42,6 +47,12 @@ let shuffledColors = shuffle(COLORS);
 // it creates a new div and gives it a class with the value of the color
 // it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
+  // score element
+  const scoreEl = document.createElement('section');
+  scoreEl.innerText = score;
+  scoreEl.id = 'score';
+  gameContainer.appendChild(scoreEl);
+
   for (let color of colorArray) {
     // create a new div
     const newDiv = document.createElement("div");
@@ -59,9 +70,65 @@ function createDivsForColors(colorArray) {
 
 // TODO: Implement this function!
 function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
+  if (currentCards.length === 2) {
+    return;
+  }
+
+  const cardColor = event.target.classList[0];
+  event.target.style.backgroundColor = cardColor;
+  currentCards.push({
+    color: cardColor,
+    element: event.target
+  });
+
+  score++;
+  const scoreEl = document.getElementById('score');
+  scoreEl.innerText = score;
+
+  if (currentCards.length === 2) {
+    if (currentCards[0].element === currentCards[1].element) {
+      currentCards.splice(currentCards[1], 1);
+      return;
+    };
+
+    if (currentCards[0].color === currentCards[1].color) {
+      for (const card of currentCards) {
+        card.element.style.pointerEvents = 'none';
+      }
+      currentCards.length = 0;
+      pairsMatched++; 
+      
+      if (pairsMatched === 5) {
+        const completedEl = document.createElement('section');
+        const spanEl = document.createElement('span');
+        spanEl.innerText = `YOU FINISHED! Click "Restart" to play again!`;
+        completedEl.appendChild(spanEl);
+
+        const restartBtnEl = document.createElement('button');
+        restartBtnEl.innerText = 'Restart';
+        completedEl.appendChild(restartBtnEl);
+
+        gameContainer.appendChild(completedEl);
+
+        completedEl.addEventListener('click', function() {
+          gameContainer.innerHTML = '';
+          createDivsForColors(shuffledColors);
+        })
+      }
+      return;
+    } else {
+      setTimeout(() => {
+        for (const card of currentCards) {
+          card.element.style.backgroundColor = 'transparent';
+        }
+        currentCards.length = 0;
+      }, 1000);
+    }
+  };
 }
 
 // when the DOM loads
-createDivsForColors(shuffledColors);
+startBtn.addEventListener('click', function() {
+  gameContainer.innerHTML = '';
+  createDivsForColors(shuffledColors);
+});
